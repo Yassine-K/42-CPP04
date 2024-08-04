157c9bfbe6b8959cbf0800d3e4add35685ccface
Yak <yassinekhayri123@gmail.com>
2024-08-04 09:00:53 +0100

    Ex02 Done

---
+++ ex02/Animal.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Animal.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ykhayri <ykhayri@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/03 14:46:26 by ykhayri           #+#    #+#             */
+/*   Updated: 2024/08/04 08:58:24 by ykhayri          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+#include <iostream>
+#include "Brain.hpp"
+
+class Animal {
+	protected:
+		std::string type;
+	
+	public:
+		Animal();
+		Animal(const Animal &);
+		Animal &operator=(const Animal &);
+		virtual ~Animal();
+		virtual void makeSound() const = 0;
+		std::string getType() const;
+};
---
+++ ex02/Cat.cpp
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Cat.cpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ykhayri <ykhayri@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/03 15:12:39 by ykhayri           #+#    #+#             */
+/*   Updated: 2024/08/04 08:56:32 by ykhayri          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Cat.hpp"
+
+Cat::Cat() {
+	type = "Cat";
+	std::cout << "A Cat spawned!" << std::endl;
+	mokhi = new Brain();
+}
+
+Cat::Cat(const Cat &spawn) {
+	type = spawn.type;
+	std::cout << "A Cat cloned!" << std::endl;
+}
+
+Cat &Cat::operator=(const Cat &spawn) {
+	if (this != &spawn) {
+		type = spawn.type;
+	}
+	std::cout << "A Cat op cloned!" << std::endl;
+	return *this;
+}
+
+Cat::~Cat() {
+	delete mokhi;
+	std::cout << "Cat dispawned!" << std::endl;
+}
+
+void Cat::makeSound() const{
+	std::cout << "Tha Cat is meowing!" << std::endl;
+}
